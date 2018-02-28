f90e0cf9f4dade2f9ec326fe17abe2c2cae78c23
omar reid <omar.reid@verizon.net>
2018-02-28 06:56:13 -0700

    API Now Work

--- YourFitness.Business/YourFitness.Business/Controllers/UserController.cs
+++ YourFitness.Business/YourFitness.Business/Controllers/UserController.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using YourFitness.Business.Models;
 
 namespace YourFitness.Business.Controllers
 {
-    [Produces("application/json")]
-    [System.Web.Http.Route("api/User")]
-    public class UserController : ApiController
-    {
-        public bool Create(User U)
-        {
-            return true;
-        }
+   [Controller]
+   [Produces("application/json")]
+   [Route("api/User")]
+   public class UserController
+   {
+      public string Index()
+      {
+         return "Hello from user controller";
+      }
 
-    }
+   }
 }
--- YourFitness.Business/YourFitness.Business/Startup.cs
+++ YourFitness.Business/YourFitness.Business/Startup.cs
@@ -1,34 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace YourFitness.Business
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+   public class Startup
+   {
+      public IConfigurationRoot Configuration;
+      /// <summary>
+      /// Make resouces available to DI container and middleware
+      ///   such as appsettings and environment variables
+      /// </summary>
+      /// <param name="env"></param>
+      public Startup(IHostingEnvironment env)
+      {
+         var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+         Configuration = builder.Build();
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddMvc();
+      }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      {
+         //if (env.IsDevelopment())
+         //{
+         //   app.UseDeveloperExceptionPage();
+         //   app.UseBrowserLink();
+         //}
+
+         //app.UseStaticFiles();
+
+         //app.UseMvc(routes => {
+         //   routes.MapRoute(
+         //      name: "default",
+         //      template: "{controller}/{action}/{id?}",
+         //      defaults: new {controller = "User", action = "Index" }
+         //   );
+         //});
+
+         //app.UseRouter(
+         //   route => {
+         //      route.MapRoute(
+         //          name: "Default",
+         //          template: "api/{controller}/{action}/{id?}",
+         //          defaults: new { controller = "User" }
+         //      );
+         //   }
+         //).UseMvcWithDefaultRoute();
+
+         //app.Run(async (context) =>
+         //{
+         //      await context.Response.WriteAsync("Hello World!");
+         //});
+
+         app.UseMvc();
+      }
+   }
 }
