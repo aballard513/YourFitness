26785a8836afc2f0932ecd7545ed259a472f8bfd
Eric <ericjchristie@gmail.com>
2018-03-14 19:48:18 -0700

    installed react-router-dom and added BrowserRouter to index.js

--- YourFitness.Client/index.js
+++ YourFitness.Client/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './containers/App';
 
-
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
 
 ReactDOM.render(
 <Provider store={store}>
-<App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 </Provider>, 
 
 
