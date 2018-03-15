c9407ef0e08b612cb2bd4cccbcd3a1e392fa9ea9
Andre Ballard <andre.ballard@outlook.com>
2018-03-14 23:17:49 -0700

    removed coments

--- YourFitness.Client/components/UserView.js
+++ YourFitness.Client/components/UserView.js
@@ -9,8 +9,6 @@ export default class UserView extends React.Component {
       this.state = {view : "initial", objs : []}
       this.checkState = this.checkState.bind(this);
       this.ViewUsers = this.ViewUsers.bind(this);
-      //this.render = this.render.bind(this);
-      //var Users  {string:name}
     }
 
     ViewUsers(e){
@@ -20,18 +18,11 @@ export default class UserView extends React.Component {
         axios({
           method: 'get',
           url: 'http://localhost:57515/api/user',
-          //data: bodyFormData,
           config: { headers: {'Content-Type': 'multipart/form-data' }}
         })
         .then((response) => {
           //handle success
           this.setState( {objs : response.data});
-          //this.setState({view: "users"});
-         
-          //return obj;*/
-          //var d = response.data
-          //console.log(d[0].firstName);
-          console.log("success")
       })
       .catch(function (response) {
           //handle error;
@@ -39,7 +30,6 @@ export default class UserView extends React.Component {
       });
 
       this.setState({view: "users"});
-      //console.log(this.state.objs);
       }
 
       checkState(){
@@ -51,7 +41,7 @@ export default class UserView extends React.Component {
             return <div><button onClick={this.ViewUsers}>ViewUsers</button></div>
         }
         else{
-            //console.log("userview")
+            
             let el = this.state.objs.map(objects =>
                 <div key ={objects.id} > {objects.firstName} {objects.lastName} </div>
             )
