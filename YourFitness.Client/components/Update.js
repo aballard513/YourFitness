4d98eb1313364c002895498519409299d2d916c2
Andre Ballard <andre.ballard@outlook.com>
2018-03-18 04:22:07 -0700

    update form created

---
+++ YourFitness.Client/components/Update.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {connect} from 'react-redux';
+
+
+export default class Update extends React.Component {
+
+    constructor(props){
+      super(props);
+      
+    }
+    
+    render(){
+        <form>
+        <div>
+            <label>FirstName</label>
+            
+            <label>LastName</label>
+            
+        </div>
+        </form>
+    }
+}
--- YourFitness.Client/components/UserView.js
+++ YourFitness.Client/components/UserView.js
@@ -2,15 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import store from '../store'
 import HomeView from './Home.js'
+import Update from './Update.js'
+import User from '../reducers/User';
 
 export default class UserView extends React.Component {
 
+    
     constructor(props){
       super(props);
-      this.state = {view : "initial", objs : []}
+      this.state = {view : "initial", objs : [], updateduser : {}, id: null}
       this.checkState = this.checkState.bind(this);
       this.ViewUsers = this.ViewUsers.bind(this);
       this.Register = this.Register.bind(this);
+      this.UpdateUser = this.UpdateUser.bind(this);
     }
 
     ViewUsers(e){
@@ -51,6 +55,12 @@ export default class UserView extends React.Component {
                 this.setState({view : "initial"})
             }
         }
+
+    UpdateUser(evt){
+        this.setState({id: evt.target.id})
+        this.setState({view : "update"})
+    }
+
     render(){
         if(this.checkState() == "initial"){
             return <div><button onClick={this.ViewUsers}>View All Users</button></div>
@@ -63,11 +73,37 @@ export default class UserView extends React.Component {
                     </div>
                     )
         }
+        else if(this.checkState() == "update")
+        {
+            var userid = this.state.id;
+            var users = this.state.objs;
+            var user = users[userid-1];
+            let form = (
+            <div>
+            <h1>Update Information</h1>
+            <form>
+            <div>
+                <label>FirstName</label>
+                <input type="text" name="firstName" placeholder={user.firstName} /><br/>
+                <label>Last Name</label>
+                <input type="text" name="lastName" placeholder={user.lastName} /><br/>
+                <label>Email</label>
+                <input type="text" name="email" placeholder= {user.email} /><br/>
+            </div>
+            </form>
+            </div>
+            )
+            return (
+                <div>
+                        {form}
+                </div>
+
+            )
+        }
         else
         {
-            
             let el = this.state.objs.map(objects =>
-                <div key ={objects.id} > {objects.firstName} {objects.lastName} <button onClick={this.UpdateUser}>update</button></div>
+                <div key ={objects.id} > {objects.firstName} {objects.lastName} <button id ={objects.id} onClick={this.UpdateUser}>update</button></div>
             )
             
             return(<div>
