093a0294ffa0de155cee0d4bcb44d5dd1b5b92d5
Andre Ballard <andre.ballard@outlook.com>
2018-03-24 20:46:55 -0700

    added src folder and edited configuration to output files in dist folder. use "npm start" to run app

--- YourFitness.Client/webpack/webpack.dev.config.js
+++ YourFitness.Client/webpack/webpack.dev.config.js
@@ -5,28 +5,41 @@ var parentDir = path.join(__dirname, '../');
 
 module.exports = {
     entry: [
-        path.join(parentDir, 'index.js')
+        path.join(parentDir, 'src/app/index.js'),
+        
     ],
+
+    output: {
+        path: parentDir + 'dist/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
 
     module: {
         loaders: [{
+            
             test: /\.(js|jsx)$/,
+                    include: parentDir + 'src',
                     exclude: /node_modules/,
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    query:{ 
+                        presets: ["react", "es2015", "stage-2"]}
                 },{
                     test: /\.less$/,
                     loaders: ["style-loader", "css-loader", "less-loader"]
                   }
-                ],   
+                ],    
+                
     },
 
-    output: {
-        path: parentDir + '/dist',
-        filename: 'bundle.js'
-    },
+    resolve: {
+        extensions: ['.js', '.jsx'] 
+      },
 
     devServer: {
-        contentBase: parentDir,
+        contentBase: parentDir + 'src',
         historyApiFallback:true
     }
+    
 }
+
