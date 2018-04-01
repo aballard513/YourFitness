c83fbda6a5b059a27dd516dcd6b9029aa4d0fc44
Andre Ballard <andre.ballard@outlook.com>
2018-04-01 11:53:20 -0700

    fixed switch warning

--- YourFitness.Client/src/containers/App.js
+++ YourFitness.Client/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Home from '../components/Home';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 
 import {connect} from 'react-redux';
 import HomeView from '../components/HomeView';
@@ -9,10 +9,10 @@ class App extends Component
     render(){
         return (
         <BrowserRouter>
-        <switch>
+        <Switch>
         <Route exact path='/' render={props => <Home {...props} />}/>
         <Route exact path='/Home' component = {HomeView} />
-        </switch>
+        </Switch>
         </BrowserRouter> 
         )
     }
