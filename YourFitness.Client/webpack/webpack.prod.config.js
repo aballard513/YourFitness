4d557134c17cb44b74dcaf63fe7174a5851be71d
Andre Ballard <andre.ballard@outlook.com>
2018-05-06 12:38:56 -0700

    changed public path

--- YourFitness.Client/webpack/webpack.prod.config.js
+++ YourFitness.Client/webpack/webpack.prod.config.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.config.js');
 var EncodingPlugin = require('webpack-encoding-plugin');
 var path = require('path');
 var parentDir = path.join(__dirname, '../');
-
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 
 module.exports = merge(common, {
@@ -12,14 +12,15 @@ module.exports = merge(common, {
   output: {
     path: parentDir + 'app',
     filename: '[name].js',
-    publicPath: '/app/'
+    publicPath: ASSET_PATH
 },
     plugins:[
             new webpack.optimize.CommonsChunkPlugin('common'),
             new webpack.optimize.UglifyJsPlugin(),
             new webpack.optimize.AggressiveMergingPlugin(),
             new webpack.DefinePlugin({
-                       'process.env.NODE_ENV': JSON.stringify('production')
+                       'process.env.NODE_ENV': JSON.stringify('production'),
+                       'process.env.ASSET_PATH': JSON.stringify('ASSET_PATH')
                      }),
             new EncodingPlugin({
               encoding: 'UTF-8'
